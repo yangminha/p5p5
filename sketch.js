2cb08aff85f8786ba08930be1bf2800d72d85f97
yangminha <yangminha@gmail.com>
2022-04-05 12:57:53 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  let r = map(mouseX, 0, windowWidth, 0, 255);
+  let b = map(mouseY, 0, windowWidth, 0, 255);
+  fill(r, 0, b, 20);
+  stroke(r/2, 0, b/2, 30);
+  ellipse(mouseX,mouseY, 150, 150);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
